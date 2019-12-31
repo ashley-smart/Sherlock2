ba1efe2d127e46c65f438a71b95e6a0ff79114a7
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2019-12-31 08:39:39 -0800

    adding 1218 and 1220 to jpg files

---
+++ ffmpeg_to_jpg_1218-1220.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH--partition=trc
+#SBATCH--time=2:00:00
+#SBATCH--ntasks=1
+#SBATCH--cpus-per-task=3
+#SBATCH --output=./outputs_ffmpegtovideo/slurm-%j.out
+
+#gets frame every 10 seconds
+
+ml system
+ml ffmpeg
+
+
+ffmpeg -i '/oak/stanford/groups/trc/data/Ashley2/PER/20191218/Video_1.m4v' -vf fps=1/10 '/oak/stanford/groups/trc/data/Ashley2/PER/20191218/analysis/Video1/V01frame_%07d.jpg'
+
+ffmpeg -i '/oak/stanford/groups/trc/data/Ashley2/PER/20191220/Video_1.m4v' -vf fps=1/10 '/oak/stanford/groups/trc/data/Ashley2/PER/20191220/analysis/Video1/V01frame_%07d.jpg'
