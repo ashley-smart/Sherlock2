bc1c3f5fb9987ed8ce773201edfbe4e5928e47d5
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2020-10-30 14:46:16 -0700

    Create motion_correction_all_brain_ind_ch.sh

---
+++ brain_analysis/registration/motion_correction_all_brain_ind_ch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --job-name=motion_correct
+#SBATCH --partition=trc
+#SBATCH --time=72:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --output=/home/users/asmart/scripts/Sherlock2/brain_analysis/registration/job_outputs/%x.%j.out
+#SBATCH --open-mode=append
+
+module use /home/groups/trc/modules
+module load antspy/0.2.2
+
+python3 /home/users/asmart/scripts/Sherlock2/brain_analysis/registration/motion_correction_all_brain_ind_ch.py
